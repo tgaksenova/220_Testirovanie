7d5b2d08abfd82f9b6541237a93131df6888fa38
RuslanM1102 <118619671+RuslanM1102@users.noreply.github.com>
2023-10-24 10:38:50 +0300

    just preparation for pages + database

---
+++ PR30/Database/SingletonModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace PR30.Database
+{
+    static class SingletonModel
+    {
+        private static TestingEntities _context;
+
+        public static TestingEntities GetContext()
+        {
+            return _context = _context ?? new TestingEntities();
+        }
+    }
+}
